a39acd3619f644933121732bae40deb4b1859a46
tomloake <thomas.loake@gmail.com>
2024-01-03 16:58:11 -0500

    added config for cookie banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,28 @@ import jopSoftwarecookieconsent from "@jop-software/astro-cookieconsent";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.tomloake.com",
-  integrations: [mdx(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), jopSoftwarecookieconsent()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    jopSoftwarecookieconsent({
+      gui_options: {
+        consent_modal: {
+          layout: "cloud", // box/cloud/bar
+          position: "bottom center", // bottom/middle/top + left/right/center
+          transition: "slide", // zoom/slide
+          swap_buttons: false, // enable to invert buttons
+        },
+        settings_modal: {
+          layout: "box", // box/bar
+          // position: 'left',           // left/right
+          transition: "slide", // zoom/slide
+        },
+      },
+    }),
+  ],
 });
