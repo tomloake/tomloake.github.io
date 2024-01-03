3e3c4c81fef8e8c10da5755c28a5f18f9bf17b88
tomloake <thomas.loake@gmail.com>
2024-01-03 16:44:18 -0500

    added cookie banner, doesnt load gtm in dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
-
 import sitemap from "@astrojs/sitemap";
+
+import jopSoftwarecookieconsent from "@jop-software/astro-cookieconsent";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.tomloake.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [mdx(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), jopSoftwarecookieconsent()]
 });
